7cddd4cdf78662fea50dfb7c1e0845a1be736062
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-12 18:45:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,11 @@ require 'pry'
 class Song
 
   attr_accessor :name, :artist
-
+  @@all
   def initialize(name)
     @name = name  end
     @artist = nil
+    @@all << self
     Artist.allsongs << self
     # # binding.pry
     # puts "adding #{name}"
